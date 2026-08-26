bd937e7d8e24e75e21721ef4b3d7c7935d71425b
星辰 <razy-ovo@outlook.com>
2026-08-26 19:12:43 +0800

    更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,15 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
-import swup from "@swup/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'always',
+  },
   integrations: [
     svelte(),
     icon(),
-    swup({
-      containers: ["#swup"],
-      cache: true,
-      updateHead: true,
-      theme: false,
-    }),
   ],
 });
