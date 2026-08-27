e8559e5a615357299336fb684c25fc7ed4452cb7
星辰 <razy-ovo@outlook.com>
2026-08-27 11:28:50 +0800

    修复动画

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import icon from "astro-icon";
+import swup from "@swup/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,11 @@ export default defineConfig({
   integrations: [
     svelte(),
     icon(),
+    swup({
+      containers: ["#swup"],
+      cache: true,
+      updateHead: true,
+      theme: false,
+    }),
   ],
 });
